c5049487f40351e504dbcb4aeb988f2645abe80e
Richard Anaya <richard.anaya@gmail.com>
2021-01-15 22:28:21 -0800

    fix

--- avatar.js
+++ avatar.js
@@ -5,11 +5,23 @@ let privateKey = null;
 let exportedAvatar = null;
 let currentIdentity = null;
 
+function ab2str(buf) {
+    return String.fromCharCode.apply(null, new Uint16Array(buf));
+}
+function str2ab(str) {
+    var buf = new ArrayBuffer(str.length * 2);
+    var bufView = new Uint16Array(buf);
+    for (var i = 0, strLen = str.length; i < strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+}
+
 const hasIdentityTemplate = () => html`<article class="card">
     <section>
-    <h1>Crypto Avatar</h1>
-    <p>Hello <b>${currentIdentity.name}</b></p>
-    <p>This is where you will be able to modify your avatar and export out it's data to use in virtual worlds.</p>
+        <h1>Crypto Avatar</h1>
+        <p>Hello <b>${currentIdentity.name}</b></p>
+        <p>This is where you will be able to modify your avatar and export out it's data to use in virtual worlds.</p>
     </section>
     ${firstTime ? html`
     <section>
@@ -31,17 +43,21 @@ const hasIdentityTemplate = () => html`<article class="card">
 const createIdentityTemplate = () => html`<article class="card">
     <section>
         <h1>Crypto Avatar</h1>
-        <p>Welcome to this tool for creating/modifying avatars to use on virtual worlds. No information here will be stored on any server.</p>
+        <p>Welcome to this tool for creating/modifying avatars to use on virtual worlds. No information here will be
+            stored on any server.</p>
         <h2>Are you new?</h2>
-        <p>It looks like you don't have an active avatar identity. Would you like to create one? Give us a name you'd like for us to be able to give for this identity. This is the name the world will see. Worlds may restrict name length, filter profanity or non-standard characters, so its best to choose a simple name you and others will understand.</p>
+        <p>It looks like you don't have an active avatar identity. Would you like to create one? Give us a name you'd
+            like for us to be able to give for this identity. This is the name the world will see. Worlds may restrict
+            name length, filter profanity or non-standard characters, so its best to choose a simple name you and others
+            will understand.</p>
         <input id="avatar_name" type="text" />
         <button @click="${createIdentity}">Create Avatar</button>
         <h2>I already have an identity i'd like to use</h2>
         <label for="avatar_private_key">Private Key</label>
         <textarea name="avatar_private_key" id="avatar_private_key" type="text"></textarea>
         <label for="avatar_data">Avatar Data</label>
-        <textarea name="avatar_data" id="avatar_data" type="text"></textarea><button
-            @click="${uploadIdentity}">Load Avatar</button>
+        <textarea name="avatar_data" id="avatar_data" type="text"></textarea><button @click="${uploadIdentity}">Load
+            Avatar</button>
     </section>
 </article>
 `;
@@ -52,6 +68,8 @@ function savedPrivateKey() {
 }
 
 function exit() {
+    firstTime = false;
+    exportedAvatar = null;
     privateKey = null;
     currentIdentity = null;
     window.localStorage.removeItem("neonorigami_private_key");
@@ -60,19 +78,62 @@ function exit() {
 }
 
 
-function exportAvatar() {
+async function exportAvatar() {
     console.log("Exporting")
-    exportedAvatar = "a;slkdnf;lasdkn"
+    delete currentIdentity.signature;
+    const priv = await window.crypto.subtle.importKey(
+        "jwk",
+        JSON.parse(privateKey),
+        {
+            name: "RSA-PSS",
+            hash: { name: "SHA-256" },
+        },
+        true,
+        ["sign"]
+    );
+    const sig = await window.crypto.subtle.sign(
+        {
+            name: "RSA-PSS",
+            saltLength: 128,
+        },
+        priv,
+        str2ab(JSON.stringify(currentIdentity))
+    );
+    currentIdentity.signature = ab2str(sig);
+    exportedAvatar = JSON.stringify(currentIdentity)
     renderAll()
 }
 
-function createIdentity() {
+async function createIdentity() {
+    let keys = await window.crypto.subtle.generateKey(
+        {
+            name: "RSA-PSS",
+            modulusLength: 2048,
+            publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+            hash: { name: "SHA-256" },
+        },
+        true,
+        ["sign", "verify"]
+    );
+    let pub = await window.crypto.subtle.exportKey(
+        "jwk",
+        keys.publicKey
+    );
+
+    console.log("private")
+    let priv = await window.crypto.subtle.exportKey(
+        "jwk",
+        keys.privateKey
+    );
+    console.log("done private")
+    console.log(priv)
     firstTime = true;
     const name = document.querySelector("#avatar_name").value;
-    privateKey = "abc"
+    privateKey = JSON.stringify(priv)
     window.localStorage.setItem("neonorigami_private_key", privateKey);
     window.localStorage.setItem("neonorigami_identity", JSON.stringify({
-        name
+        name,
+        public_key: pub
     }));
     renderAll()
 }

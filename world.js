67153a00ac3b1f3a65dc0e00152b9cf4412071b1
Richard Anaya <richard.anaya@gmail.com>
2021-01-16 15:11:26 -0800

    adding world

--- world.js
+++ world.js
@@ -1,3 +1,39 @@
+const gun = Gun({ peers: ['https://gunjs.herokuapp.com/gun'] });
+
+class SignalingChannel {
+    constructor(local_uuid, remote_uuid) {
+        this.lastMessageIndex = -1;
+        this.outgoing = {};
+        this.local_uuid = local_uuid;
+        this.remote_uuid = remote_uuid;
+        this.onmessage = undefined;
+    }
+
+    send(data) {
+        if (this.outgoing.length === undefined) {
+            this.outgoing.length = 0;
+        }
+        this.outgoing["message" + this.outgoing.length] = data;
+        this.outgoing.length++;
+        gun.get(this.remote_uuid).put({ content: JSON.stringify(this.outgoing) });
+    }
+
+    addListener(listener) {
+        console.log("listening for data on " + this.local_uuid);
+        this.onmessage = listener;
+        gun.get(this.local_uuid).on(d => {
+            const data = JSON.parse(d.content)
+            if (this.onmessage) {
+                while (this.lastMessageIndex < data.length - 1) {
+                    this.lastMessageIndex++;
+                    const arg = data["message" + this.lastMessageIndex];
+                    this.onmessage(arg)
+                }
+            }
+        });
+    }
+}
+
 function uuid() {
     function _p8(s) {
         var p = (Math.random().toString(16) + "000000000").substr(2, 8);
@@ -15,67 +51,130 @@ function getParameterByName(name, url = window.location.href) {
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
+let stun = getParameterByName("stun");
+
 const isServer = getParameterByName("join") === null;
 
-const gun = Gun({ peers: ['https://gunjs.herokuapp.com/gun'] });
-const peer = new RTCPeerConnection({
-    iceServers: [
-        {
-            urls: "stun:stun.l.google.com:19302"
-        }
-    ]
-});
-peer.onicecandidate = e => {
-    console.log(e)
-}
-
-function start(channel) {
-    channel.onmessage = () => {
-        console.log("data channel message");
-    };
-    channel.onopen = () => {
-        console.log("data channel open");
-    };
-    channel.onclose = () => {
-        console.log("data channel close");
-    };
-}
+let local_uuid = uuid();
+let remote_uuid = uuid();
 
 if (isServer) {
-    (async function () {
-        const offer = await peer.createOffer();
-        await peer.setLocalDescription(offer);
-        let offer_uuid = uuid();
-        let answer_uuid = uuid();
-        document.querySelector("#url").innerHTML = window.origin + window.location.pathname + `?join=gun&offer=${offer_uuid}&answer=${answer_uuid}`;
-        console.log("sending offer to " + offer_uuid);
-        gun.get(offer_uuid).put(JSON.parse(JSON.stringify(offer)));
-        console.log("waiting for answer at " + answer_uuid);
-        gun.get(answer_uuid).on(async function (data) {
-            console.log("answer received");
-            await peer.setRemoteDescription(new RTCSessionDescription(data));
-            console.log("peer handshake complete");
-            const channel = peer.createDataChannel("sendChannel");
-            console.log("data channel created");
-            start(channel);
-        })
-    })()
+    document.body.innerHTML = "Send this url to the other person: <br>" + window.origin + window.location.pathname + `?stun=${stun}&join=gun&local=${remote_uuid}&remote=${local_uuid}`;
 } else {
-    let offer_uuid = getParameterByName("offer");
-    let answer_uuid = getParameterByName("answer");
-    console.log("waiting for offer");
-    gun.get(offer_uuid).on(async function (data) {
-        console.log("offer received at " + offer_uuid);
-        await peer.setRemoteDescription(new RTCSessionDescription(data));
-        const answer = await peer.createAnswer();
-        await peer.setLocalDescription(answer);
-        console.log("sending answer to " + answer_uuid);
-        gun.get(answer_uuid).put(JSON.parse(JSON.stringify(answer)));
-        console.log("peer handshake complete");
-        peer.ondatachannel = (event) => {
-            console.log("data channel received");
-            const channel = event.channel;
-            start(channel);
-        };
-    })
+    const tmp = local_uuid;
+    local_uuid = getParameterByName("local");
+    remote_uuid = getParameterByName("remote");
 }
+
+const signaling = new SignalingChannel(local_uuid, remote_uuid);
+const configuration = { iceServers: [{ urls: 'stun:' + stun }] };
+const pc = new RTCPeerConnection(configuration);
+
+// Send any ice candidates to the other peer.
+pc.onicecandidate = ({ candidate }) => {
+    signaling.send({ candidate: JSON.parse(JSON.stringify(candidate)) });
+}
+
+// Let the "negotiationneeded" event trigger offer generation.
+pc.onnegotiationneeded = async () => {
+    try {
+        console.log("creating offer to send to client");
+        await pc.setLocalDescription(await pc.createOffer());
+        // Send the offer to the other peer.
+        signaling.send({ desc: JSON.parse(JSON.stringify(pc.localDescription)) });
+        console.log("offer sent");
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+// Once remote track media arrives, show it in remote video element.
+pc.ontrack = (event) => {
+    // Don't set srcObject again if it is already set.
+    if (remoteView.srcObject) return;
+    remoteView.srcObject = event.streams[0];
+};
+
+pc.oniceconnectionstatechange = e => console.log("Connection state:" + pc.iceConnectionState);
+
+// Call start() to initiate.
+async function start() {
+    try {
+        if (isServer) {
+            console.log('Creating data channel');
+            const dataChannel =
+                pc.createDataChannel("myLabel");
+
+            dataChannel.onerror = (error) => {
+                console.log("Data Channel Error:", error);
+            };
+
+            dataChannel.onmessage = (event) => {
+                console.log("Got Data Channel Message:", event.data);
+            };
+
+            dataChannel.onopen = () => {
+                console.log("opened")
+                document.body.innerHTML = "Sent the peer a message!"
+                dataChannel.send("Hello World!");
+            };
+
+            dataChannel.onclose = () => {
+                console.log("The Data Channel is Closed");
+            };
+        } else {
+            document.body.innerHTML = "Waiting to receive network info of server ... "
+            pc.ondatachannel = function (ev) {
+                console.log('Data channel is created!');
+                const dataChannel = ev.channel;
+                dataChannel.onerror = (error) => {
+                    console.log("Data Channel Error:", error);
+                };
+
+                dataChannel.onmessage = (event) => {
+                    document.body.innerHTML = "Got Data Channel Message:" + event.data;
+                };
+
+                dataChannel.onopen = () => {
+                    console.log("opened")
+                    document.body.innerHTML = "data channel opened with peer!"
+                };
+
+                dataChannel.onclose = () => {
+                    console.log("The Data Channel is Closed");
+                };
+            };
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+signaling.addListener(async ({ desc, candidate }) => {
+    try {
+        if (desc) {
+            // If you get an offer, you need to reply with an answer.
+            if (desc.type === 'offer') {
+                console.log("received offer");
+                await pc.setRemoteDescription(desc);
+                await pc.setLocalDescription(await pc.createAnswer());
+                console.log("sending answer");
+                document.body.innerHTML = "Connecting ... this may take awhile (i've seen this take up to a minute)"
+                signaling.send({ desc: JSON.parse(JSON.stringify(pc.localDescription)) });
+            } else if (desc.type === 'answer') {
+                console.log("received answer");
+                await pc.setRemoteDescription(new RTCSessionDescription(desc));
+                document.body.innerHTML = "A client wants to talk with us ... connecting"
+            } else {
+                console.log('Unsupported SDP type.');
+            }
+        } else if (candidate) {
+            console.log("received candidate");
+            await pc.addIceCandidate(candidate);
+        }
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+start()

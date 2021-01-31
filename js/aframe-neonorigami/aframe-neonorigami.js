0ca2744a0b93bc8fb754bb2d8cf5af72ba1f87c5
Richard Anaya <richard.anaya@gmail.com>
2021-01-30 20:51:51 -0800

    random land

--- js/aframe-neonorigami/aframe-neonorigami.js
+++ js/aframe-neonorigami/aframe-neonorigami.js
@@ -1,26 +1,57 @@
-(function () {
-  'use strict';
 
-  AFRAME.registerComponent('neon-origami-environment', {
-      schema: {
-          "sky-color": {type: 'color', default: '#87CEEB'}
-      },
-      init: function () {
-          this.didChange = true;
-      },
-      update: function (oldData) {
-          if(oldData["sky-color"] != this.data["sky-color"]){
-              this.didChange = true;
-          }
-      },
-      tick: function () {
-          if(this.didChange){
-              // get the three js scene
-              const scene = this.el.closest("a-scene").object3D;
-              scene.background = new THREE.Color( this.data["sky-color"] );
-              this.didChange = false;
-          }
-      }
+(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
+(function () {
+    'use strict';
+
+    function generateGrid(heightCalc, material){
+        const geometry = new THREE.Geometry();
+        const pointWidth = 200;
+        for(let y = -pointWidth/2; y <= pointWidth/2; y++){
+            for(let x = -pointWidth/2; x <= pointWidth/2; x++){
+                geometry.vertices.push(
+                    new THREE.Vector3(x, heightCalc(x,y), y),
+                );
+            }  
+        }
+
+        for(let y= 0; y < pointWidth; y++){
+            for(let x = 0; x < pointWidth; x++){
+                let curPoint = y*pointWidth+x; 
+                geometry.faces.push(
+                    new THREE.Face3(curPoint,curPoint+pointWidth+1,curPoint+1),
+                    new THREE.Face3(curPoint+pointWidth+1,curPoint+pointWidth+2,curPoint+1),
+                );
+            }   
+        }
+
+        return new THREE.Mesh(geometry, material);
+    }
+
+    AFRAME.registerComponent('neon-origami-environment', {
+        schema: {
+            "sky-color": { type: 'color', default: '#87CEEB' }
+        },
+        init: function () {
+            this.didChange = true;
+        },
+        update: function (oldData) {
+            if (oldData["sky-color"] != this.data["sky-color"]) {
+                this.didChange = true;
+            }
+        },
+        tick: function () {
+            if (this.didChange) {
+                // get the three js scene
+                const scene = this.el.closest("a-scene").object3D;
+                scene.background = new THREE.Color(this.data["sky-color"]);
+                this.didChange = false;
+
+                // land
+                scene.add(generateGrid(()=>2*Math.random()-1,new THREE.MeshBasicMaterial({color: new THREE.Color("#7cfc00")})));
+                // water
+                scene.add(generateGrid(()=>0,new THREE.MeshBasicMaterial({color: new THREE.Color("#0077be")})));
+            }
+        }
     });
 
 }());
--- js/aframe-neonorigami/src/environment.js
+++ js/aframe-neonorigami/src/environment.js
@@ -1,21 +1,50 @@
+function generateGrid(heightCalc, material){
+    const geometry = new THREE.Geometry();
+    const pointWidth = 200;
+    for(let y = -pointWidth/2; y <= pointWidth/2; y++){
+        for(let x = -pointWidth/2; x <= pointWidth/2; x++){
+            geometry.vertices.push(
+                new THREE.Vector3(x, heightCalc(x,y), y),
+            );
+        }  
+    }
+
+    for(let y= 0; y < pointWidth; y++){
+        for(let x = 0; x < pointWidth; x++){
+            let curPoint = y*pointWidth+x 
+            geometry.faces.push(
+                new THREE.Face3(curPoint,curPoint+pointWidth+1,curPoint+1),
+                new THREE.Face3(curPoint+pointWidth+1,curPoint+pointWidth+2,curPoint+1),
+            );
+        }   
+    }
+
+    return new THREE.Mesh(geometry, material);
+}
+
 AFRAME.registerComponent('neon-origami-environment', {
     schema: {
-        "sky-color": {type: 'color', default: '#87CEEB'}
+        "sky-color": { type: 'color', default: '#87CEEB' }
     },
     init: function () {
         this.didChange = true;
     },
     update: function (oldData) {
-        if(oldData["sky-color"] != this.data["sky-color"]){
+        if (oldData["sky-color"] != this.data["sky-color"]) {
             this.didChange = true;
         }
     },
     tick: function () {
-        if(this.didChange){
+        if (this.didChange) {
             // get the three js scene
             const scene = this.el.closest("a-scene").object3D;
-            scene.background = new THREE.Color( this.data["sky-color"] );
+            scene.background = new THREE.Color(this.data["sky-color"]);
             this.didChange = false;
+
+            // land
+            scene.add(generateGrid(()=>2*Math.random()-1,new THREE.MeshBasicMaterial({color: new THREE.Color("#7cfc00")})));
+            // water
+            scene.add(generateGrid(()=>0,new THREE.MeshBasicMaterial({color: new THREE.Color("#0077be")})));
         }
     }
-  });
+});
